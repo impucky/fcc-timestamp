3f151d98b8d4d1bb873d56c205279e11e292c5a4
impucky <raphael.bgrd@gmail.com>
2017-03-31 13:14:13 +0200

    functional microservice + landing page

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var path = require('path');
+var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.get('/:date', function (req, res) {
+    var q = req.params.date;
+    var output = { "unix": null, "natural": null };
+
+    // check for numeric date
+    if (!isNaN(q)) {
+        q = parseInt(q);
+    }
+
+    var d = new Date(q);
+
+    // on valid date, fill output object
+    if (!isNaN(d.getTime())) {
+        var month = d.toLocaleString('en-us', { month: 'long' });
+        output.unix = d.getTime();
+        output.natural = `${month} ${d.getDate()}, ${d.getFullYear()}` 
+    }
+    // send output
+    res.send(JSON.stringify(output));
 })
+    
+app.listen(3000);
